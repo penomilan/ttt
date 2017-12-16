0d49bae18471b6ba3e2bafa990e778c47ca4482e
penomilan <penomilan@gmail.com>
2017-12-16 15:51:15 +0100

    goin home

---
+++ js/ttt.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+        var cells;
+
+        function refresh() {
+            $("#playground").empty("table");
+            var table = $("<table>");
+            var tr;
+            $.each(cells, function(i) {
+                if (!(i % 3) || !tr) {
+                    tr = $("<tr>");
+                };
+                var td = $("<td>");
+                td.attr("id", "cell_" + i);
+                td.html(cells[i]);
+                td.click(function() {
+                    $("#result").text("You've clicked on cell number " + $(this).attr("id").split("_")[1] + ".");
+                })
+                table.append(tr.append(td));
+            });
+            $("#playground").append(table);
+        }
+
+        $("#start").click(function() {
+            cells = ["", "", "", "", "", "", "", "", ""];
+            refresh();
+        })
+
+    })
